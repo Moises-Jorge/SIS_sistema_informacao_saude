e283cce8bf4bac70ef910ffb567a1d42fd6b5392
Moisés Jorge <moisesjorge30@gmail.com>
2024-02-03 16:47:21 +0100

    Criação da migration Agendamento

---
+++ database/migrations/2024_02_03_153704_create_agendamentos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agendamentos', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->time('hora');
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('pessoal_clinico_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('exame_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('consulta_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('especialidade_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agendamentos');
+    }
+};
