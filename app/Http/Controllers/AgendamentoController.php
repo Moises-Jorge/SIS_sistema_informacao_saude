94e96595ccc7db801311c7b8470179d14df95f56
Moisés Jorge <moisesjorge30@gmail.com>
2024-02-16 10:59:06 +0100

    Improved the AgendamentoController

--- app/Http/Controllers/AgendamentoController.php
+++ app/Http/Controllers/AgendamentoController.php
@@ -91,6 +91,30 @@ class AgendamentoController extends Controller
                 'consulta_id' => $request->input("consulta_id")
             ]);
         }
+
+        $todos_agendamentos = Agendamento::join("pessoal__clinicos", "pessoal__clinicos.id", "=", "agendamentos.pessoal__clinico_id")
+            ->join("users", "pessoal__clinicos.user_id", "=", "users.id")
+            ->join("especialidades", "especialidades.id", "=", "pessoal__clinicos.especialidade_id")
+            ->select(
+                'users.nome as nomePessoalClinico',
+                'especialidades.nome as nome_especialidade',
+                'agendamentos.*'
+            )
+            ->where("agendamentos.user_id", "=", 1)
+            ->orderBy('agendamentos.data', 'asc')
+            ->orderBy('agendamentos.hora', 'asc')
+            ->get();
+
+
+        $todas_consultas = Consulta::join('especialidades', 'especialidades.id', '=', 'consultas.especialidade_id')
+            ->select('consultas.*', 'especialidades.nome as nomeEspecialidade')->get(); //all();
+
+        $todos_exames = Exame::join('especialidades', 'especialidades.id', '=', 'exames.especialidade_id')
+            ->select('exames.*', 'especialidades.nome as nomeEspecialidade')->get(); //all();
+
+
+
+        return view('site.admin.appointment-list', compact('todos_agendamentos', 'todas_consultas', 'todos_exames'))->with('success', 'Cadastro realizado com sucesso!');
     }
 
     /**
