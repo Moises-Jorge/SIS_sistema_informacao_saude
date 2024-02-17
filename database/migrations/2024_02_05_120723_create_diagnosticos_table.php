5983b0cb0d214575887e24fc2ee87aef7bc816f2
Moisés Jorge <moisesjorge30@gmail.com>
2024-02-17 15:51:52 +0100

    modified an atribut in diagnosticos_table

--- database/migrations/2024_02_05_120723_create_diagnosticos_table.php
+++ database/migrations/2024_02_05_120723_create_diagnosticos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('tipo_doenca'); // Alergica ou Nao Alergica
             $table->string('nome')->nullable(); // Nome da doenca se ela for Nao Alergica
             $table->string('estado'); // Estado do paciente perante a doenca diagnosticada
-            $table->date('data'); // Data em que a doenca foi diagnosticada
+            $table->date('data')->nullable(); // Data em que a doenca foi diagnosticada
             $table->string('descricao');
             $table->foreignId('reg__clinico__utente_id')->constrained()->cascadeOnDelete();
             $table->foreignId('pessoal__clinico_id')->constrained()->cascadeOnDelete();
