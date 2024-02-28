2399cec1d2987dabc588069d9ced65cb5838aa63
Moisés Jorge <moisesjorge30@gmail.com>
2024-02-28 19:12:07 +0100

    melhorei o main_header

--- resources/views/site/main_header.blade.php
+++ resources/views/site/main_header.blade.php
@@ -9,13 +9,15 @@
                 </span>
             </a>
             <a href="index-2.html" class="navbar-brand logo">
-                <img src="assets/img/logo.png" class="img-fluid" alt="Logo">
+                {{-- <img src="assets/img/logo.png" class="img-fluid" alt="Logo"> --}}
+                <img src="{{asset('assets/img/logo.png')}}" class="img-fluid" alt="Logo">
             </a>
         </div>
         <div class="main-menu-wrapper">
             <div class="menu-header">
                 <a href="index-2.html" class="menu-logo">
-                    <img src="assets/img/logo.png" class="img-fluid" alt="Logo">
+                    {{-- <img src="assets/img/logo.png" class="img-fluid" alt="Logo"> --}}
+                    <img src="{{asset('assets/img/logo.png')}}" class="img-fluid" alt="Logo">
                 </a>
                 <a id="menu_close" class="menu-close" href="javascript:void(0);">
                     <i class="fas fa-times"></i>
