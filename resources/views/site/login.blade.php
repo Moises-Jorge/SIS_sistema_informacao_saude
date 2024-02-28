cbc30cebd4aa5a7c8d8190253d9d373f18bc28ab
Moisés Jorge <moisesjorge30@gmail.com>
2024-02-28 20:04:03 +0100

    melhorando a pagina de login

--- resources/views/site/login.blade.php
+++ resources/views/site/login.blade.php
@@ -74,8 +74,8 @@
 							<i class="far fa-hospital"></i>
 						</div>
 						<div class="header-contact-detail">
-							<p class="contact-header">Contact</p>
-							<p class="contact-info-header"> +1 315 369 5943</p>
+							<p class="contact-header">SIS-UAN </p>
+							<p class="contact-info-header">+244 952 573 924</p>
 						</div>
 					</li>
 					<li class="nav-item">
@@ -147,124 +147,7 @@
 		<!-- /Page Content -->
 
 		<!-- Footer -->
-		<footer class="footer">
-
-			<!-- Footer Top -->
-			<div class="footer-top">
-				<div class="container-fluid">
-					<div class="row">
-						<div class="col-lg-3 col-md-6">
-
-							<!-- Footer Widget -->
-							<div class="footer-widget footer-about">
-								<div class="footer-logo">
-									<img src="{{asset('assets/img/footer-logo.png')}}" alt="logo">
-								</div>
-								<div class="footer-about-content">
-									<p>Seu bem-estar, nosso dever. </p>
-									<div class="footer-widget footer-contact">
-
-
-									</div>
-									<div class="social-icon">
-
-
-										<ul>
-											<li>
-												<a href="#" target="_blank"><i class="fab fa-facebook-f"></i> </a>
-											</li>
-											<li>
-												<a href="#" target="_blank"><i class="fab fa-twitter"></i> </a>
-											</li>
-											<li>
-												<a href="#" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-											</li>
-											<li>
-												<a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
-											</li>
-											<li>
-												<a href="#" target="_blank"><i class="fab fa-dribbble"></i> </a>
-											</li>
-										</ul>
-
-
-									</div>
-								</div>
-							</div>
-							<!-- /Footer Widget -->
-
-						</div>
-
-						<div class="col-lg-3 col-md-6">
-
-
-						</div>
-
-						<div class="col-lg-3 col-md-6">
-
-
-
-						</div>
-
-						<div class="col-lg-3 col-md-6">
-
-							<!-- Footer Widget -->
-							<div class="footer-widget footer-contact">
-								<h2 class="footer-title">Contacto</h2>
-								<div class="footer-contact-info">
-									<div class="footer-address">
-										<span><i class="fas fa-map-marker-alt"></i></span>
-										<p> 3556 Beech Street, San Francisco,<br> California, CA 94108 </p>
-									</div>
-									<p>
-										<i class="fas fa-phone-alt"></i>
-										+1 315 369 5943
-									</p>
-									<p class="mb-0">
-										<i class="fas fa-envelope"></i>
-										doccure@example.com
-									</p>
-								</div>
-							</div>
-							<!-- /Footer Widget -->
-
-						</div>
-
-					</div>
-				</div>
-			</div>
-			<!-- /Footer Top -->
-
-			<!-- Footer Bottom -->
-			<div class="footer-bottom">
-
-
-
-				<div class="container-fluid">
-
-					<!-- Copyright -->
-					<div class="copyright">
-						<div class="row">
-
-							<!-- Copyright Menu -->
-							<div class="copyright-menu text-center">
-								<ul class="policy-menu">
-
-									<li><a href="#">&copy Copyrigh 2024 PDC.AO </a></li>
-									<li><a href="#">SIS</a></li>
-								</ul>
-							</div>
-							<!-- /Copyright Menu -->
-
-						</div>
-					</div>
-					<!-- /Copyright -->
-
-				</div>
-			</div>
-			<!-- /Footer Bottom -->
-
-		</footer>
+			@include('site.main_footer')
 		<!-- /Footer -->
 
 	</div>
--- resources/views/site/main_footer.blade.php
+++ resources/views/site/main_footer.blade.php
@@ -1,41 +1,119 @@
-<div class="footer-top">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-3 col-md-6">
-            
-                <!-- Footer Widget -->
-                <div class="footer-widget footer-about">
-                    <div class="footer-logo">
-                        <img src="assets/img/footer-logo.png" alt="logo">
-                    </div>
-                <div class="footer-about-content">
-                    <p>Seu bem-estar, nosso dever. </p>
-                <div class="footer-widget footer-contact">
-                        
+<footer class="footer">
+				
+    <!-- Footer Top -->
+    <div class="footer-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-about">
+                        <div class="footer-logo">
+                            {{-- <img src="assets/img/footer-logo.png" alt="logo"> --}}
+                            <img src="{{asset('assets/img/footer-logo.png')}}" alt="logo">
+                        </div>
+                        <div class="footer-about-content">
+                            <p>Seu bem-estar, nosso dever. </p>
+                            <div class="footer-widget footer-contact">
                             
-                </div>
-                <div class="social-icon">
+                                
+                            </div>
+                            <div class="social-icon">
 
+                                
+                                <ul>
+                                    <li>
+                                        <a href="facebook.com" target="_blank"><i class="fab fa-facebook-f"></i> </a>
+                                    </li>
+                                    {{-- <li>
+                                        <a href="twitter.com" target="_blank"><i class="fab fa-twitter"></i> </a>
+                                    </li> --}}
+                                    <li>
+                                        <a href="linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                                    </li>
+                                    <li>
+                                        <a href="instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                                    </li>
+                                    {{-- <li>
+                                        <a href="#" target="_blank"><i class="fab fa-dribbble"></i> </a>
+                                    </li> --}}
+                                </ul>
                             
-                    <ul>
-                        <li>
-                            <a href="facebook.com" target="_blank"><i class="fab fa-facebook-f"></i> </a>
-                        </li>
-                        {{-- <li>
-                            <a href="twitter.com" target="_blank"><i class="fab fa-twitter"></i> </a>
-                        </li> --}}
-                        <li>
-                            <a href="linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                        </li>
-                        <li>
-                            <a href="instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        {{-- <li>
-                            <a href="#" target="_blank"><i class="fab fa-dribbble"></i> </a>
-                        </li> --}}
-                    </ul>
-                        
-                            
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+                    
+                </div>
+                
+                <div class="col-lg-3 col-md-6">
+                
+                    
+                </div>
+                
+                <div class="col-lg-3 col-md-6">
+                
+                
+                    
+                </div>
+                
+                <div class="col-lg-3 col-md-6">
+                
+                    <!-- Footer Widget -->
+                    <div class="footer-widget footer-contact">
+                        <h2 class="footer-title">Contacto</h2>
+                        <div class="footer-contact-info">
+                            <div class="footer-address">
+                                <span><i class="fas fa-map-marker-alt"></i></span>
+                                <p> Luanda, Belas,<br> Camama-1 </p>
+                            </div>
+                            <p>
+                                <i class="fas fa-phone-alt"></i>
+                                +244 952 573 924
+                            </p>
+                            <p class="mb-0">
+                                <i class="fas fa-envelope"></i>
+                                sisuan@example.com
+                            </p>
+                        </div>
+                    </div>
+                    <!-- /Footer Widget -->
+                    
+                </div>
+                
+            </div>
         </div>
     </div>
-</div>
+    <!-- /Footer Top -->
+    
+    <!-- Footer Bottom -->
+    <div class="footer-bottom">
+        
+
+
+        <div class="container-fluid">
+        
+            <!-- Copyright -->
+            <div class="copyright">
+                <div class="row">
+            
+                        <!-- Copyright Menu -->
+                        <div class="copyright-menu text-center">
+                            <ul class="policy-menu">
+                                
+                                <li><a href="#">&copy Copyrigh 2024 PDC.AO </a></li>
+                                <li><a href="#">SIS</a></li>
+                            </ul>
+                        </div>
+                        <!-- /Copyright Menu -->
+        
+                </div>
+            </div>
+            <!-- /Copyright -->
+            
+        </div>
+    </div>
+    <!-- /Footer Bottom -->
+    
+</footer>
